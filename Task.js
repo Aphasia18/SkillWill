a4846d7dde212177f5de222dccd47abdfa975688
Guga <gugakakashvili1337@gmail.com>
2023-10-28 13:59:58 +0400

    npm task, validator

---
+++ Task.js
@@ -0,0 +1,13 @@
+// მოახდინე პროექტის ინიციალიზაცია
+// დააინსტალირე პაკეტი validator
+// მოიძიე validator-ის დოკუმენტაცია
+// npmjs.com - ზე
+// დოკუმენტაციის მიხედვით გამოიყენე
+// ბრძანება isEmail და დაბეჭდე არის თუ არა
+// შემდეგი სიტყვები მეილი:
+// test@test.com, abcDE123
+
+var validator = require("validator");
+
+console.log(validator.isEmail("test@test.com"));
+console.log(validator.isEmail("abcDE123"));
