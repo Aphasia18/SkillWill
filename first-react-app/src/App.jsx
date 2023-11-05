55a0e7c6cba11ee521f610f82f10d702f9b8614b
Guga <gugakakashvili1337@gmail.com>
2023-11-05 13:51:35 +0400

    first react app Primogem!

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import primo from "./Primogem.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <img src={primo} alt="" className="star" />
+      <h1>This is Primogem</h1>
+      <p>Primogem is shiny</p>
+      <p>Primogem is rare</p>
+      <p>Primogem makes wishes come true!</p>
+    </div>
+  );
+}
+
+export default App;
