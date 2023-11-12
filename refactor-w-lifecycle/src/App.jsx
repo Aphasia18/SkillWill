c06486c4ee50153594c53c134109485b6152cf46
Guga <gugakakashvili1337@gmail.com>
2023-11-12 15:14:06 +0400

    refactor using lifecycle methods

---
+++ refactor-w-lifecycle/src/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import "./App.css";
+import TaskList from "./components/TaskList";
+
+class App extends Component {
+  render() {
+    return <TaskList />;
+  }
+}
+
+export default App;
